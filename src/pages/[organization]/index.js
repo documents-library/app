3c588c707056a32e11dbb26d8959e49994cb5cd6
Gabriel Salas <ggsalas@gmail.com>
2020-02-09 23:17:26 -0300

    get sites from be

--- src/pages/[organization]/index.js
+++ src/pages/[organization]/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import fetch from 'isomorphic-unfetch'
 
 import OrganizationHomePage from '../../components/site/OrganizationHomePage'
 
@@ -14,60 +15,19 @@ Organization.propTypes = {
 
 Organization.getInitialProps = async ctx => {
   const { organization } = ctx.query
-  // const res = await fetch(`http://localhost:8080/sites/${siteName}`)
-  // const { site } = await res.json()
+  const res = await fetch(`${process.env.API_URL}/sites`)
+  const sites = await res.json()
+
   // TODO: add organizations to BE
-  const mockedResponse = {
-    organization: {
-      name: 'taller@',
-      longName: 'Taller Vertical de Construcciones "M"',
-      description:
-        'Cátedras de Construcciones 1 a 3 de la Facultad de Arquitectura Urbanismo y Diseño de la Universidad Nacional de Mar del Plata.'
-    },
-    sites: [
-      {
-        _id: '5d0820fdba4d5a14d185c3dd',
-        name: 'my.site',
-        owner: '5cf2de7c6845b57f6a3f3f20',
-        longName: 'Sitio con nombre largo',
-        description: 'Description of the site',
-        googleFolderId: '181KuCL0DZ_UtetYpO4h4mMC4MoVnUm9V',
-        createdAt: '2019-06-17T23:23:41.977Z',
-        updatedAt: '2019-06-17T23:23:41.977Z',
-        __v: 0,
-        organizationName: 'taller@'
-      },
-      {
-        _id: '5d0820fdba4d5a14d185c3dd',
-        name: 'C1',
-        owner: '5cf2de7c6845b57f6a3f3f20',
-        longName: 'Construcciones 1',
-        description:
-          'Cátedra de Construcciones 1 de la Facultad de Arquitectura Urbanismo y Diseño de la Universidad Nacional de Mar del Plata."M"',
-        googleFolderId: '181KuCL0DZ_UtetYpO4h4mMC4MoVnUm9V',
-        createdAt: '2019-06-17T23:23:41.977Z',
-        updatedAt: '2019-06-17T23:23:41.977Z',
-        __v: 0,
-        organizationName: 'taller@'
-      },
-      {
-        _id: '5d0820fdba4d5a14d185c3dd',
-        name: 'C2',
-        longName: 'Construcciones 2',
-        description:
-          'Cátedra de Construcciones 2 de la Facultad de Arquitectura Urbanismo y Diseño de la Universidad Nacional de Mar del Plata."M"',
-        owner: '5cf2de7c6845b57f6a3f3f20',
-        googleFolderId: '181KuCL0DZ_UtetYpO4h4mMC4MoVnUm9V',
-        createdAt: '2019-06-17T23:23:41.977Z',
-        updatedAt: '2019-06-17T23:23:41.977Z',
-        __v: 0,
-        organizationName: 'taller@'
-      }
-    ]
+  const mockedOrganization = {
+    name: 'taller@',
+    longName: 'Taller Vertical de Construcciones "M"',
+    description:
+      'Cátedras de Construcciones 1 a 3 de la Facultad de Arquitectura Urbanismo y Diseño de la Universidad Nacional de Mar del Plata.'
   }
 
   return {
-    organization: mockedResponse.organization,
-    sites: mockedResponse.sites
+    organization: mockedOrganization,
+    sites
   }
 }
