190fda8cfbcab4fa6e23144e5de85234eb78ebc1
Gabriel Salas <ggsalas@gmail.com>
2020-04-27 20:23:31 +0200

    fix(components): remove disabled statis of link

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,30 +1,33 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 import SuiLink from '@s-ui/react-router/lib/Link'
-/* global caches */
 
 export default function Link({to, children, ...rest}) {
-  const [disabled, setDisabled] = useState(false)
-  const isOnline = navigator.onLine
-
-  useEffect(() => {
-    if (isOnline) return
-
-    async function hasLinkInCache() {
-      try {
-        const cache = await caches.open('v1')
-        const cachedLink = await cache.match(to.pathname)
-        const hasCachedLink = Boolean(cachedLink)
-
-        setDisabled(!hasCachedLink)
-      } catch (e) {
-        console.log('get cache error: ', e) // eslint-disable-line
-      }
-    }
-
-    hasLinkInCache()
-  }, [isOnline, to.pathname])
+  const disabled = false
+  // const [disabled, setDisabled] = useState(false)
+  // const isOnline = navigator.onLine
+  //
+  // useEffect(() => {
+  //   if (isOnline) return
+  //
+  //   async function hasLinkInCache() {
+  //     try {
+  //       debugger // eslint-disable-line
+  //       const cache = await caches.open(window.documentsLi?.cacheName ?? '')
+  //       const cachedLink = await cache.match(to.pathname)
+  //       const hasCachedLink = Boolean(cachedLink)
+  //
+  //       console.log('cacheLink: ', cachedLink, 'hasCachedLink: ', hasCachedLink)
+  //
+  //       setDisabled(!hasCachedLink)
+  //     } catch (e) {
+  //       console.log('get cache error: ', e) // eslint-disable-line
+  //     }
+  //   }
+  //
+  //   hasLinkInCache()
+  // }, [isOnline, to.pathname])
 
   function renderChildren() {
     if (typeof children === 'function') {
